26a1c752c75961092050bc535473f60241b6b344
Gleydson Rodrigues <rodrigues.gw@gmail.com>
2022-09-15 00:39:13 +0200

    UP add and configuring to style

--- web/tailwind.config.cjs
+++ web/tailwind.config.cjs
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    './src/**/*.tsx','./index.html'
+  ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage:{
+        'nlw-gradient':'linear-gradient(89.86deg, #9572FC 03.08%, #43E7AD 53.94%, #E1D55D 97.57%)',
+        galaxy: "url('/background-galaxy.jpeg')",
+        'game-gradient': 'linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.9) 67.08%)',
+      }
+    },
   },
   plugins: [],
 }
+ 
