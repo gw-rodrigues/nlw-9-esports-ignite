338e515229ca6bd67f81e85c5e0322190c2cc39e
Gleydson Rodrigues <rodrigues.gw@gmail.com>
2022-09-15 23:23:20 +0200

    UP importing Home screen and Loading component

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -8,7 +8,9 @@ import {
   Inter_900Black,
 } from "@expo-google-fonts/inter";
 
+import { Home } from "./src/screens/Home";
 import { Background } from "./src/components/Background";
+import { Loading } from "./src/components/Loading";
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -24,7 +26,7 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-      {fontsLoaded ? "" : ""}
+      {fontsLoaded ? <Home /> : <Loading />}
     </Background>
   );
 }
