f5b7287ae3874eeda3ab920029009a6bd9404e23
Gleydson Rodrigues <rodrigues.gw@gmail.com>
2022-09-15 23:50:53 +0200

    ADD game list component

---
+++ mobile/src/components/GameCard/index.tsx
@@ -0,0 +1,36 @@
+import {
+  TouchableOpacity,
+  TouchableOpacityProps,
+  ImageBackground,
+  ImageSourcePropType,
+  Text,
+} from "react-native";
+
+import { LinearGradient } from "expo-linear-gradient";
+
+import { styles } from "./styles";
+import { THEME } from "../../theme";
+
+export interface GameCardProps {
+  id: string;
+  name: string;
+  ads: string;
+  cover: ImageSourcePropType;
+}
+
+interface Props extends TouchableOpacityProps {
+  data: GameCardProps;
+}
+
+export function GameCard({ data, ...rest }: Props) {
+  return (
+    <TouchableOpacity style={styles.container}>
+      <ImageBackground source={data.cover} style={styles.cover}>
+        <LinearGradient style={styles.footer} colors={THEME.COLORS.FOOTER}>
+          <Text style={styles.name}>{data.name}</Text>
+          <Text style={styles.ads}>{data.ads} anúncios</Text>
+        </LinearGradient>
+      </ImageBackground>
+    </TouchableOpacity>
+  );
+}
