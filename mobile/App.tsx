abe7ab12bf8de7a1f0583fb6d8dbc5aae06f8185
Gleydson Rodrigues <rodrigues.gw@gmail.com>
2022-09-15 23:12:54 +0200

    UP add background and statusbar

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,20 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "react-native";
+
+import { useFonts } from "@expo-google-fonts/inter";
+
+import { Background } from "./src/components/Background";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Background>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+    </Background>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
