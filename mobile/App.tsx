92917ebcdfe3b26ab2e227be47c3e7c6c4d2d4cb
Gleydson Rodrigues <rodrigues.gw@gmail.com>
2022-09-15 23:15:29 +0200

    UP importing fonts and useFonts

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,10 +1,22 @@
 import { StatusBar } from "react-native";
 
-import { useFonts } from "@expo-google-fonts/inter";
+import {
+  useFonts,
+  Inter_400Regular,
+  Inter_600SemiBold,
+  Inter_700Bold,
+  Inter_900Black,
+} from "@expo-google-fonts/inter";
 
 import { Background } from "./src/components/Background";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_600SemiBold,
+    Inter_700Bold,
+    Inter_900Black,
+  });
   return (
     <Background>
       <StatusBar
@@ -12,6 +24,7 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
+      {fontsLoaded ? "" : ""}
     </Background>
   );
 }
