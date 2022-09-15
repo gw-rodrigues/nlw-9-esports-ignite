d3a77b8ae3934b4b76bd24cdb2777bd56da46d79
Gleydson Rodrigues <rodrigues.gw@gmail.com>
2022-09-15 23:36:47 +0200

    UP importing heading and styles

--- mobile/src/screens/Home/index.tsx
+++ mobile/src/screens/Home/index.tsx
@@ -1,8 +1,17 @@
 import React from "react";
-import { View } from "react-native";
-
+import { View, Image } from "react-native";
+import logoImg from "../../assets/logo-nlw-esports.png";
+import { Heading } from "../../components/Heading";
 import { styles } from "./styles";
 
 export function Home() {
-  return <View style={styles.container}></View>;
+  return (
+    <View style={styles.container}>
+      <Image source={logoImg} style={styles.logo} />
+      <Heading
+        title="Encontre seu duo!"
+        subtitle="Selecione o game que deseja jogar..."
+      />
+    </View>
+  );
 }
