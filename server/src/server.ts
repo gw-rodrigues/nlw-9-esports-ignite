b95f8e0af909dba3801292b7743d37f2370eba64
Gleydson Rodrigues <rodrigues.gw@gmail.com>
2022-09-16 21:21:12 +0200

    UP add routes and dynamic params

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,13 +2,25 @@ import express from "express";
 
 const app = express();
 
-app.get("/ads", (request, response) => {
+app.get("/games", (request, response) => {
+  return response.json([]);
+});
+
+app.post("/games", (request, response) => {
+  return response.status(201).json([]);
+});
+
+app.get("/games/:id/ads", (request, response) => {
   return response.json([
     { id: 1, name: "Anuncio 1" },
     { id: 2, name: "Anuncio 2" },
     { id: 3, name: "Anuncio 3" },
     { id: 4, name: "Anuncio 4" },
   ]);
+});
+
+app.get("/games/:id/discord", (request, response) => {
+  return response.json([]);
 });
 
 app.listen(3333);
