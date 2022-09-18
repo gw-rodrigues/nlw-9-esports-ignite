9c52fc76c6a7b1a21f5453f29be6c96e5dc00c0b
Gleydson Rodrigues <rodrigues.gw@gmail.com>
2022-09-18 17:27:30 +0200

    UP comments

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,38 @@
+/**
+ * !!! quando usamos o prisma com "tsnd ..." no package.json devemos adicionar mais um comando
+ * comando completo:
+ *
+ *  "dev": "tsnd --exit-child src/server.ts",
+ *
+ * conexão com database previne a reinicialização do nosso server, assim fechará todas conexões, reiniciar e cria um nova conexão
+ */
 import express from "express";
+import { PrismaClient } from "@prisma/client";
 
+/**
+ * Iniciar rotas com express
+ */
 const app = express();
+/**
+ * Ciar nova conexão do prisma com database
+ * - pode receber objetos de configurações
+ * - neste caso vamos usar logs para poder identificar falhas/erros/outro
+ * log: ["query"] -> mostra todas a query feitas
+ */
+const prisma = new PrismaClient({ log: ["query"] });
+/**
+ * Alguns métodos no prisma sao assíncrono, sendo assim devemos usar async/awawit
+ * ex: game.findMany() -> assíncrono método
+ *
+ * na função adicionamos os async e no método do prisma o await
+ */
 
-app.get("/games", (request, response) => {
-  return response.json([]);
+app.get("/games", async (request, response) => {
+  const games = await prisma.game.findMany();
+  /**
+   * como o retorno prisma e uma lista, de games, podemos retornar diretamente no .json(games)
+   */
+  return response.json(games);
 });
 
 app.post("/games", (request, response) => {
