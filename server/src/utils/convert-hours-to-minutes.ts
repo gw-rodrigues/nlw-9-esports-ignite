037275f07f281eb35bf311bf115b4d6a1d02379c
Gleydson Rodrigues <rodrigues.gw@gmail.com>
2022-09-18 22:18:38 +0100

    ADD hours to minutes

--- server/src/utils/convert-hours-to-minutes.ts
+++ server/src/utils/convert-hours-to-minutes.ts
@@ -1,5 +1,9 @@
-//recebe 15:00
-//convert 1080
-function convertHourToMinutes(hourString: string) {
+//recebe "15:00"
+// -> ["15","00"]
+//convert [15, 00]
+//return 1500
+export function convertHoursToMinutes(hourString: string) {
   const [hours, minutes] = hourString.split(":").map(Number);
+  const minutesAmount = hours * 60 + minutes;
+  return minutesAmount;
 }
