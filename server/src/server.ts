c51c80ad8e45d7eebb56feb079c93c1b58e02bbd
Gleydson Rodrigues <rodrigues.gw@gmail.com>
2022-09-18 18:05:45 +0200

    UP create ads to database

--- server/src/server.ts
+++ server/src/server.ts
@@ -11,8 +11,12 @@ import { PrismaClient } from "@prisma/client";
 
 /**
  * Iniciar rotas com express
+ *
+ * Indicamos ao express para usar método JSON
  */
 const app = express();
+app.use(express.json());
+
 /**
  * Ciar nova conexão do prisma com database
  * - pode receber objetos de configurações
@@ -28,28 +32,114 @@ const prisma = new PrismaClient({ log: ["query"] });
  */
 
 app.get("/games", async (request, response) => {
-  const games = await prisma.game.findMany();
+  const games = await prisma.game.findMany({
+    //search all
+    include: {
+      // = join
+      _count: {
+        // agradador count / sera novo campo / key - value no json
+        select: {
+          //campo/relacionamento ads
+          ads: true,
+        },
+      },
+    },
+  });
   /**
    * como o retorno prisma e uma lista, de games, podemos retornar diretamente no .json(games)
    */
   return response.json(games);
 });
 
-app.post("/games", (request, response) => {
-  return response.status(201).json([]);
+app.post("/games/:id/ads", async (request, response) => {
+  /**
+   * nessa rota vamos criar os ads para os game.
+   *
+   * quando for fazer inserção, cardaste sensíveis usamos:
+   * request body
+   *
+   * valores recebidos através do método post
+   * {
+   *   "name": "fake-name-1",
+   *   "yearsPlaying": 1,
+   *   "discord": "fake-discord#000",
+   *   "weekdays": [4,5,6],
+   *   "hourStart": 1080,
+   *   "hourEnd": 1320,
+   *   "useVoiceChannel": true
+   * }
+   */
+  const gameId = request.params.id;
+  const body = request.body;
+
+  const ad = prisma.ad.create({
+    data: {
+      gameId,
+      name: body.name,
+      yearsPlaying: body.yearsPlaying,
+      discord: body.discord,
+      weekDays: body.weeDays.join(","),
+      hourStar: body.hourStar,
+      hourEnd: body.hourEnd,
+      useVoiceChannel: body.useVoiceChannel,
+    },
+  });
+
+  return response.status(201).json(body);
 });
 
-app.get("/games/:id/ads", (request, response) => {
-  return response.json([
-    { id: 1, name: "Anuncio 1" },
-    { id: 2, name: "Anuncio 2" },
-    { id: 3, name: "Anuncio 3" },
-    { id: 4, name: "Anuncio 4" },
-  ]);
+app.get("/games/:id/ads", async (request, response) => {
+  const gameId = request.params.id;
+  const ads = await prisma.ad.findMany({
+    select: {
+      //select = quais campos queremos mostrar
+      id: true,
+      name: true,
+      weeDays: true,
+      useVoiceChannel: true,
+      yearsPlaying: true,
+      hourStart: true,
+      hourEnd: true,
+    },
+    where: {
+      //where val = database field value
+      gameId, //como params gameId e igual a campo da database, passamos apenas gameId, se nao era -{ gameId : id}
+    },
+    orderBy: {
+      // ordem dos valores mostrado, sendo desc os últimos adicionados
+      createdAt: "desc",
+    },
+  });
+
+  /**
+   * Iremos formatar os valores recebidos na hora retorno
+   */
+  return response.json(
+    ads.map((ad: any) => {
+      return {
+        ...ad, //pegamos todos os valores
+        weekDays: ad.weeDays.split(","), //mas alteramos apenas esse key-value do ad / como e uma string vamos fazer split para passar para um array, cada dia da semana
+      };
+    })
+  );
 });
 
-app.get("/ads/:id/discord", (request, response) => {
-  return response.json([]);
+app.get("/ads/:id/discord", async (request, response) => {
+  const adId = request.params.id;
+  /**
+   * prisma findUnique - pode receber valores inválidos, como null or invalid, que pode causar erros.
+   * prisma findUniqueOrThrow - caso for valor invalido disparamos um error, nao retornamos a response.
+   */
+  const ad = await prisma.ad.findUniqueOrThrow({
+    select: {
+      // selecionamos apenas o campo discord para ser retornado da query
+      discord: true,
+    },
+    where: {
+      id: adId,
+    },
+  });
+  return response.json({ discord: ad.discord });
 });
 
 app.listen(3333);
