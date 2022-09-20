05377f7643e36c1c404443bd291c8e84097fbee8
Gleydson Rodrigues <rodrigues.gw@gmail.com>
2022-09-20 18:26:21 +0100

    ADD game page

---
+++ mobile/src/screens/Game/index.tsx
@@ -0,0 +1,7 @@
+import { SafeAreaView } from "react-native-safe-area-context";
+
+import { styles } from "./styles";
+
+export function Game() {
+  return <SafeAreaView style={styles.container}></SafeAreaView>;
+}
