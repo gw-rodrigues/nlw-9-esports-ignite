6ddb2a891930067434855c1706c54f455029f2a1
Gleydson Rodrigues <rodrigues.gw@gmail.com>
2022-09-20 18:10:52 +0100

    UP FIX using temporally old games request

--- mobile/src/screens/Home/index.tsx
+++ mobile/src/screens/Home/index.tsx
@@ -1,12 +1,25 @@
+import { useEffect, useState } from "react";
 import { View, Image, FlatList } from "react-native";
 import logoImg from "../../assets/logo-nlw-esports.png";
-import { GameCard } from "../../components/GameCard";
+import { GameCard, GameCardProps } from "../../components/GameCard";
 import { Heading } from "../../components/Heading";
 import { styles } from "./styles";
 
 import { GAMES } from "../../utils/games";
 
 export function Home() {
+  const [games, setGames] = useState<GameCardProps[]>([]);
+  useEffect(() => {
+    try {
+      fetch("http://localhost:3333/games")
+        .then((response) => response.json())
+        .then((data) => setGames(data));
+    } catch (error) {
+      console.log(error);
+    }
+    setGames(GAMES);
+  }, []);
+
   return (
     <View style={styles.container}>
       <Image source={logoImg} style={styles.logo} />
@@ -15,7 +28,7 @@ export function Home() {
         subtitle="Selecione o game que deseja jogar..."
       />
       <FlatList
-        data={GAMES}
+        data={games}
         keyExtractor={(item) => item.id}
         renderItem={({ item }) => <GameCard data={item} />}
         horizontal
