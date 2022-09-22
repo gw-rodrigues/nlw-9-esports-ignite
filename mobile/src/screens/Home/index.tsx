67188fd5ee26222b5cfdcaf1d49641b9028bd2af
Gleydson Rodrigues <rodrigues.gw@gmail.com>
2022-09-22 15:41:10 +0200

    UP import use navigation

--- mobile/src/screens/Home/index.tsx
+++ mobile/src/screens/Home/index.tsx
@@ -5,6 +5,7 @@ import { Image, FlatList } from "react-native";
  * SafeAreaView -> ajuda evitar areas view de diversos dispositivos, como botoes e cortes/design da tela.
  */
 import { SafeAreaView } from "react-native-safe-area-context";
+import { useNavigation } from "@react-navigation/native";
 
 import logoImg from "../../assets/logo-nlw-esports.png";
 import { GameCard, GameCardProps } from "../../components/GameCard";
@@ -27,6 +28,21 @@ export function Home() {
     setGames(GAMES);
   }, []);
 
+  /**
+   * useNavigation() -> recebemos as rotas, e opções de navegação
+   *  - navigation.nagigate("game"); -> conseguimos navegar entre páginas
+   */
+  const navigation = useNavigation();
+  /**
+   * Consoante a navigation.d.ts, como definimos tipagem, teremos que passar os valores
+   * na função handleOpenGame para poder receber na nova view/stack
+   * passamos no navigation as informações necessária, como rota e params
+   * -> navigation.nagigate("game", { id, title, bannerUrl });
+   */
+  function handleOpenGame({ id, title, bannerUrl }: GameCardProps) {
+    navigation.nagigate("game", { id, title, bannerUrl });
+  }
+
   return (
     <Background>
       <SafeAreaView style={styles.container}>
@@ -38,7 +54,9 @@ export function Home() {
         <FlatList
           data={games}
           keyExtractor={(item) => item.id}
-          renderItem={({ item }) => <GameCard data={item} />}
+          renderItem={({ item }) => (
+            <GameCard data={item} onPress={() => handleOpenGame(item)} />
+          )}
           horizontal
           showsHorizontalScrollIndicator={false}
           contentContainerStyle={styles.contentList}
