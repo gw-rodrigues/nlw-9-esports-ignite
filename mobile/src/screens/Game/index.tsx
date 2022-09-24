c684fd8b16228800bffa17cc9024eff5095c8943
Gleydson Rodrigues <rodrigues.gw@gmail.com>
2022-09-24 21:42:30 +0200

    ADD game screen

--- mobile/src/screens/Game/index.tsx
+++ mobile/src/screens/Game/index.tsx
@@ -1,13 +1,14 @@
 import { SafeAreaView } from "react-native-safe-area-context";
-import { Background } from "../../components/Background";
-
 /**
  * Para podermos obter as informações de navegação e os params
  * teremos importar o useRoutes.
  *
  * Importaremos as tipagens do GameParams da navigation.d.ts
  */
-import { useRoute } from "@react-navigation/native";
+import { useNavigation, useRoute } from "@react-navigation/native";
+
+import logoImg from "../../assets/logo-nlw-esports.png";
+
 import { GameParams } from "../../@types/navigation";
 
 /**
@@ -16,20 +17,32 @@ import { GameParams } from "../../@types/navigation";
 import { Entypo } from "@expo/vector-icons";
 
 import { styles } from "./styles";
-import { TouchableOpacity, View } from "react-native";
+import { TouchableOpacity, View, Image } from "react-native";
+import { THEME } from "../../theme";
+import { Background } from "../../components/Background";
 
 export function Game() {
   const route = useRoute();
   const game = route.params as GameParams;
+  console.log(game);
+
+  const navigation = useNavigation();
 
   return (
     <Background>
       <SafeAreaView style={styles.container}>
         <View style={styles.header}>
-          <TouchableOpacity></TouchableOpacity>
+          <TouchableOpacity>
+            <Entypo
+              name="chevron-thin-left"
+              color={THEME.COLORS.CAPTION_300}
+              size={20}
+              onPress={() => navigation.goBack()}
+            />
+          </TouchableOpacity>
+          <Image source={logoImg} style={styles.logo} />
         </View>
       </SafeAreaView>
-      ;
     </Background>
   );
 }
--- mobile/src/screens/Game/styles.ts
+++ mobile/src/screens/Game/styles.ts
@@ -1,6 +1,14 @@
 import { StyleSheet } from "react-native";
 
 export const styles = StyleSheet.create({
-  container: {},
-  header: {},
+  container: { fle: 1, alignItems: "center" },
+  header: {
+    width: "100%",
+    flexDirection: "row",
+    alignItems: "center",
+    paddingHorizontal: 32,
+    marginTop: 28,
+    justifyContent: "space-between",
+  },
+  logo: { width: 72, height: 40 },
 });
