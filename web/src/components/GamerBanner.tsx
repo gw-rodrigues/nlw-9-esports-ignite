20d73a3d83197565804938e762bdc5b49e1363b2
Gleydson Rodrigues <rodrigues.gw@gmail.com>
2022-09-27 01:03:29 +0200

    UP keen slider style

--- web/src/components/GamerBanner.tsx
+++ web/src/components/GamerBanner.tsx
@@ -5,7 +5,7 @@ interface GameBannerProps {
 }
 export function GameBanner({ bannerUrl, title, adsCount }: GameBannerProps) {
   return (
-    <a href="" className="relative rounded-lg overflow-hidden">
+    <a href="" className="relative rounded-lg overflow-hidden keen-slider__slide">
       <img src={bannerUrl} alt="" />
       <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
         <strong className="font-bold text-white block">{title}</strong>
