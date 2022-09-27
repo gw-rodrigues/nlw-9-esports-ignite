5800005b4f4e7bb864cd101bc676a218c27fdf5b
Gleydson Rodrigues <rodrigues.gw@gmail.com>
2022-09-28 00:04:50 +0200

    UP forwardRef and interface

--- web/src/components/Form/Checkbox.tsx
+++ web/src/components/Form/Checkbox.tsx
@@ -1,24 +1,32 @@
+import { forwardRef } from "react";
 import { Check } from "phosphor-react";
 import * as CheckboxRadix from "@radix-ui/react-checkbox";
-import { Controller } from "react-hook-form";
+import { Controller, FieldValues } from "react-hook-form";
 
-export function Checkbox(props: any) {
-  return (
-    <Controller
-      {...props}
-      render={({ field }) => (
-        <CheckboxRadix.Root
-          {...field}
-          value={undefined}
-          checked={field.value}
-          onCheckedChange={field.onChange}
-          className="w-6 h-6 p-1 rounded bg-zinc-900"
-        >
-          <CheckboxRadix.Indicator>
-            <Check className="w-4 h-4 text-emerald-400" />
-          </CheckboxRadix.Indicator>
-        </CheckboxRadix.Root>
-      )}
-    />
-  );
+interface CheckBoxProps {
+  name: string;
+  control: FieldValues | any;
 }
+
+export const Checkbox = forwardRef<HTMLDivElement, CheckBoxProps>(
+  ({ ...rest }: CheckBoxProps, forwardedRef) => {
+    return (
+      <Controller
+        {...rest}
+        render={({ field }) => (
+          <CheckboxRadix.Root
+            {...field}
+            value={undefined}
+            checked={field.value}
+            onCheckedChange={field.onChange}
+            className="w-6 h-6 p-1 rounded bg-zinc-900"
+          >
+            <CheckboxRadix.Indicator>
+              <Check className="w-4 h-4 text-emerald-400" />
+            </CheckboxRadix.Indicator>
+          </CheckboxRadix.Root>
+        )}
+      />
+    );
+  }
+);
