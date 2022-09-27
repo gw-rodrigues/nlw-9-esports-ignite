cbc9200cc4ec1597891bffc33ed26fa366d06df3
Gleydson Rodrigues <rodrigues.gw@gmail.com>
2022-09-27 23:57:36 +0200

    UP using Select component

--- web/src/components/Form/Select.tsx
+++ web/src/components/Form/Select.tsx
@@ -1,72 +1,92 @@
-import { Check } from "phosphor-react";
-import * as SelectRadix from "@radix-ui/react-select";
+import { forwardRef, ReactNode } from "react";
+import { Check, ArrowDown, ArrowUp } from "phosphor-react";
+import * as SelectPrimitive from "@radix-ui/react-select";
 import { Controller, FieldValues } from "react-hook-form";
-import { Children, ReactNode } from "react";
 
 interface SelectProps {
+  defaultValue?: string | undefined;
+  id: string;
   name: string;
+  label?: string;
   control: FieldValues | any;
   children: ReactNode;
-  defaultValue?: string | undefined;
-  label: string;
 }
 
-function SelectComponent({
-  children,
-  label,
-  defaultValue,
-  ...rest
-}: SelectProps) {
-  return (
-    <Controller
-      {...rest}
-      render={({ field }) => (
-        <SelectRadix.Root {...field} defaultValue={defaultValue}>
-          <SelectRadix.Trigger className="bg-zinc-900 py-3 px-4 rounded text-sm text-left placeholder:text-zinc-500">
-            <SelectRadix.Value placeholder="Selecione o game que deseja jogar" />
-          </SelectRadix.Trigger>
+export const Select = forwardRef<HTMLButtonElement, SelectProps>(
+  ({ children, label, ...rest }: SelectProps, forwardedRef) => {
+    return (
+      <Controller
+        {...rest}
+        render={({ field }) => (
+          <SelectPrimitive.Root
+            value={field.value}
+            onValueChange={field.onChange}
+          >
+            <SelectPrimitive.Trigger
+              ref={forwardedRef}
+              className="bg-zinc-900 py-3 px-4 rounded text-sm text-left placeholder:text-zinc-500 flex justify-between items-center"
+            >
+              <SelectPrimitive.Value placeholder={label} />
+              <SelectPrimitive.Icon>
+                <ArrowDown className="text-violet-500 w-4 h-4" />
+              </SelectPrimitive.Icon>
+            </SelectPrimitive.Trigger>
 
-          <SelectRadix.Portal>
-            <SelectRadix.Content className=" bg-zinc-900 rounded border border-zinc-700">
-              <SelectRadix.ScrollUpButton />
-              <SelectRadix.Viewport className="p-5">
-                {children}
-              </SelectRadix.Viewport>
-              <SelectRadix.ScrollDownButton />
-            </SelectRadix.Content>
-          </SelectRadix.Portal>
-        </SelectRadix.Root>
-      )}
-    />
-  );
+            <SelectPrimitive.Portal>
+              <SelectPrimitive.Content className="bg-zinc-900 rounded border border-zinc-700">
+                <SelectPrimitive.ScrollUpButton className="flex items-center justify-center p-2 border border-zinc-800">
+                  <ArrowUp className="text-violet-500" />
+                </SelectPrimitive.ScrollUpButton>
+
+                <SelectPrimitive.Viewport className="p-5">
+                  {children}
+                </SelectPrimitive.Viewport>
+
+                <SelectPrimitive.ScrollDownButton className="flex items-center justify-center p-2 border border-zinc-800">
+                  <ArrowDown className="text-violet-500 w-4 h-4" />
+                </SelectPrimitive.ScrollDownButton>
+              </SelectPrimitive.Content>
+            </SelectPrimitive.Portal>
+          </SelectPrimitive.Root>
+        )}
+      />
+    );
+  }
+);
+
+interface SelectItemProps {
+  value: string;
+  children: ReactNode;
 }
 
-SelectComponent.Group = SelectRadix.Group;
+export const SelectItem = forwardRef<HTMLDivElement, SelectItemProps>(
+  ({ children, ...props }: SelectItemProps, forwardedRef) => {
+    return (
+      <SelectPrimitive.Item
+        {...props}
+        ref={forwardedRef}
+        className="text-sm text-white rounded flex items-center h-6 px-8 my-1 relative select-none cursor-pointer hover:text-violet-200 hover:bg-violet-500"
+      >
+        <SelectPrimitive.ItemText>{children}</SelectPrimitive.ItemText>
+        <SelectPrimitive.ItemIndicator className="absolute left-0 w-5 inline-flex items-center justify-center">
+          <Check className="text-violet-500" />
+        </SelectPrimitive.ItemIndicator>
+      </SelectPrimitive.Item>
+    );
+  }
+);
 
 interface GroupLabelProps {
   children: ReactNode;
 }
-SelectComponent.GroupLabel = ({ children }: GroupLabelProps) => (
-  <SelectRadix.Label className=" text-sm text-zinc-400 flex items-center h-6 px-4 my-1 relative">
+export const SelectGroup = ({ children }: GroupLabelProps) => (
+  <SelectPrimitive.Group className="pb-2">
     {children}
-  </SelectRadix.Label>
+  </SelectPrimitive.Group>  
 );
 
-SelectComponent.Item = (props: any) => {
-  const { value, text, ...rest } = props;
-  return (
-    <SelectRadix.Item
-      key={value}
-      value={value}
-      {...rest}
-      className="text-sm text-white rounded flex items-center h-6 px-8 my-1 relative select-none cursor-pointer hover:text-violet-200 hover:bg-violet-500"
-    >
-      <SelectRadix.ItemText>{text}</SelectRadix.ItemText>
-      <SelectRadix.ItemIndicator className="absolute left-0 w-5 inline-flex items-center justify-center">
-        <Check className="text-violet-500" />
-      </SelectRadix.ItemIndicator>
-    </SelectRadix.Item>
-  );
-};
-
-export { SelectComponent as Select };
+export const SelectGroupLabel = ({ children }: GroupLabelProps) => (
+  <SelectPrimitive.Label className=" text-sm text-zinc-400 flex items-center h-6 px-4 my-1 relative">
+    {children}
+  </SelectPrimitive.Label>
+);
