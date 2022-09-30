eb9e1e073287cdab443e499e666a44585f179512
Gleydson Rodrigues <rodrigues.gw@gmail.com>
2022-09-30 11:14:15 +0200

    UP custom animation

--- web/tailwind.config.cjs
+++ web/tailwind.config.cjs
@@ -12,6 +12,17 @@ module.exports = {
         'nlw-gradient':'linear-gradient(89.86deg, #9572FC 03.08%, #43E7AD 53.94%, #E1D55D 97.57%)',
         galaxy: "url('/background-galaxy.jpeg')",
         'game-gradient': 'linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.9) 67.08%)',
+        'lr-gradient': 'linear-gradient(90deg, rgba(0, 0, 0, 1) 0%, rgba(0, 0, 0, .5) 80%, rgba(0, 0, 0, 0) 100%)',
+        'rl-gradient': 'linear-gradient(90deg, rgba(0, 0, 0, 0) 0, rgba(0, 0, 0, .5) 20%, rgba(0, 0, 0, 1) 100%)',
+      },
+      keyframes: {
+        show: {
+          '0%': { opacity: 0 },
+          '100%': { opacity: 1 },
+        }
+      },
+      animation: {
+        show: 'show 1s ease-in-out',
       }
     },
   },
