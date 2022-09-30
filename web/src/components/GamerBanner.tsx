0037aceb1fa4a73dfa3dba28cbe8dafa4ea574a9
Gleydson Rodrigues <rodrigues.gw@gmail.com>
2022-09-30 11:14:35 +0200

    UP hover style

--- web/src/components/GamerBanner.tsx
+++ web/src/components/GamerBanner.tsx
@@ -5,7 +5,10 @@ interface GameBannerProps {
 }
 export function GameBanner({ bannerUrl, title, adsCount }: GameBannerProps) {
   return (
-    <a href="" className="relative rounded-lg overflow-hidden keen-slider__slide">
+    <a
+      href=""
+      className="relative rounded-lg overflow-hidden keen-slider__slide animate-show hover:border border-violet-500 hover:saturate-200"
+    >
       <img src={bannerUrl} alt="" />
       <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
         <strong className="font-bold text-white block">{title}</strong>
