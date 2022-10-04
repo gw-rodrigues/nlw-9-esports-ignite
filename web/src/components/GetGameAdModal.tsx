784c0447c8193d90c52b660a83f631ef94733e08
Gleydson Rodrigues <rodrigues.gw@gmail.com>
2022-10-04 16:14:14 +0200

    UP structure

--- web/src/components/GetGameAdModal.tsx
+++ web/src/components/GetGameAdModal.tsx
@@ -1,16 +1,11 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 import * as Dialog from "@radix-ui/react-dialog";
+import { CopyToClipboard } from "react-copy-to-clipboard";
+import { useKeenSlider } from "keen-slider/react";
+import { CaretLeft, CaretRight, SpinnerGap } from "phosphor-react";
 
 import { Game } from "../App";
-import { KeenSliderPlugin, useKeenSlider } from "keen-slider/react";
-import {
-  CaretLeft,
-  CaretRight,
-  SpinnerGap,
-  User,
-  UserCircle,
-} from "phosphor-react";
 
 interface GetGameAdModal {
   game: Game;
@@ -29,8 +24,10 @@ interface GameAds {
 
 export default function GetGameAdModal({ game }: GetGameAdModal) {
   const [gameAds, setGameAds] = useState<GameAds[]>([]);
-
   const [isGameAdsLoaded, setIsGameAdsLoaded] = useState(false);
+  const [copyToClipboard, setCopyToClipboard] = useState<String | undefined>(
+    undefined
+  );
 
   useEffect(() => {
     if (!game) return;
@@ -57,11 +54,10 @@ export default function GetGameAdModal({ game }: GetGameAdModal) {
       },
     });
 
-  console.log(gameAds);
   return (
     <Dialog.Portal>
       <Dialog.Overlay className="bg-black/60 inset-0 fixed" />
-      <Dialog.Content className="fixed bg-[#2A2634] py-8 px-10 text-white top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-lg w-[480px]">
+      <Dialog.Content className="fixed overflow-auto bg-[#2A2634] py-8 px-10 text-white top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-lg w-[480px]">
         <div className="w-[100%] h-52 overflow-hidden my-4 rounded-lg">
           <img
             className="w-[100%] h-auto"
@@ -75,52 +71,111 @@ export default function GetGameAdModal({ game }: GetGameAdModal) {
         </Dialog.Title>
         <h3>Conecte-se e comece a jogar!</h3>
 
-        <div className="w-[100%] relative mt-10 flex justify-center items-center">
+        <div className="w-[100%] relative mt-5 flex justify-center items-center">
           <div className=" w-[60%]">
             {gameAds && isGameAdsLoaded ? (
               <div className="keen-slider z-0 relative" ref={gameAdsSliderRef}>
                 {gameAds.map((ads: GameAds) => (
-                  <div key={ads.id} className="keen-slider__slide">
-                    <UserCircle className=" w-14 h-14 text-zinc-300 mx-auto" />
-                    <strong className="w-[100%] text-center font-bold text-white block mx-auto">
+                  <div key={ads.id} className="keen-slider__slide p-2 px-4">
+                    <strong className="w-[100%] text-lg text-center font-bold text-white block mx-auto">
                       {ads.name}
                     </strong>
-                    <div className="mt-2 min-h-[150px] flex flex-col gap-2 bg-violet-500 hover:bg-violet-600 rounded p-4 text-white">
-                      <p>
-                        <span className="text-sm text-zinc-300 pr-2">
-                          Joga há
-                        </span>
+                    <div className="min-h-[150px] flex flex-col gap-2 rounded text-white">
+                      <p className="text-xs text-center border-t m-1 border-violet-600 pt-2">
+                        Jogando há
                         {ads.yearsPlaying === 1
-                          ? `${ads.yearsPlaying} ano`
-                          : `${ads.yearsPlaying} anos`}
+                          ? ` ${ads.yearsPlaying} ano`
+                          : ` ${ads.yearsPlaying} anos`}
                       </p>
 
-                      <p>
-                        <span className="text-sm text-zinc-300 pr-2">
-                          Dias semana:
-                        </span>
-                        {ads.weekDays.sort().toString()}
-                      </p>
+                      <div className="flex flex-wrap gap-1">
+                        {ads.weekDays.sort().map((day) => {
+                          switch (day) {
+                            case "0":
+                              return (
+                                <span className=" bg-violet-500 rounded p-1 text-sm">
+                                  domingo
+                                </span>
+                              );
+                              break;
+                            case "1":
+                              return (
+                                <span className=" bg-violet-500 rounded p-1 text-sm mx-1">
+                                  segunda
+                                </span>
+                              );
+                              break;
+                            case "2":
+                              return (
+                                <span className=" bg-violet-500 rounded p-1 text-sm">
+                                  terça
+                                </span>
+                              );
+                              break;
+                            case "3":
+                              return (
+                                <span className=" bg-violet-500 rounded p-1 text-sm">
+                                  quarta
+                                </span>
+                              );
+                              break;
+                            case "4":
+                              return (
+                                <span className=" bg-violet-500 rounded p-1 text-sm">
+                                  quinta
+                                </span>
+                              );
+                              break;
+                            case "5":
+                              return (
+                                <span className=" bg-violet-500 rounded p-1 text-sm">
+                                  sexta
+                                </span>
+                              );
+                              break;
+                            case "6":
+                              return (
+                                <span className=" bg-violet-500 rounded p-1 text-sm">
+                                  sábado
+                                </span>
+                              );
+                              break;
 
-                      <p>
-                        <span className="text-sm text-zinc-300 pr-2">
-                          Entre
-                        </span>
+                            default:
+                              break;
+                          }
+                        })}
+                      </div>
+
+                      <p className="bg-violet-700 rounded text-xs p-1 text-center">
                         {`${ads.hourStart} as ${ads.hourEnd}`}
                       </p>
 
-                      <p>
+                      <p className="text-center">
                         <span className="text-sm text-zinc-300 pr-2">
                           Usa chat de voz:
                         </span>
-                        {ads.useVoiceChannel ? "sim" : "nao"}
+                        {ads.useVoiceChannel ? <span className="text-green-500 text-xs">SIM</span> : <span className="text-red-500 text-xs">NAO</span>}
                       </p>
 
-                      <p>
-                        <span className="text-sm text-zinc-300 pr-2">
-                          Discord:
-                        </span>
+                      <p className="text-center border-t mt-2 border-violet-600 pt-2">
                         {ads.discord}
+                        <div className="w-[100%] text-center relative">
+                          <CopyToClipboard
+                            text={ads.discord}
+                            onCopy={() => setCopyToClipboard(ads.discord)}
+                          >
+                            <button className=" rounded bg-violet-700 hover:bg-violet-500 p-2 my-1 mx-auto font-bold">
+                              {copyToClipboard === ads.discord ? (
+                                <span className="text-green-500">
+                                  Discord copiado!
+                                </span>
+                              ) : (
+                                "Copiar discord?"
+                              )}
+                            </button>
+                          </CopyToClipboard>
+                        </div>
                       </p>
                     </div>
                   </div>
@@ -164,7 +219,7 @@ export default function GetGameAdModal({ game }: GetGameAdModal) {
             )}
         </div>
 
-        <div className="m-6">
+        <div className="m-4">
           Existe{" "}
           <span
             className={`font-bold mx-1 ${
